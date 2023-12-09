56c872fa44e63c97ba2d7aa2a0efd7fc7feb87a3
dacarte22 <dacarte@umich.edu>
2023-12-09 13:45:54 -0500

    asdf

---
+++ classPractice.py
@@ -0,0 +1,8 @@
+class Animals(Animate):
+    def breathe(self):
+        print('breathing')
+    def move(self):
+        print('moving')
+    def eat_food(self):
+        print('eating food')
+    
--- draw.py
+++ draw.py
@@ -1,3 +1,15 @@
 import turtle
+
+#Setup Screen
+
+screen = turtle.Screen()
+
+
 t = turtle.Pen()
 t.forward(50)
+t.left(90)
+
+
+#Click to exit to prevent instant window closing
+screen.exitonclick()
+
